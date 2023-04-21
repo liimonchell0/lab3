ad5fff1f4fa52e4c77be165a01f40b1a3e99539f
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-21 23:16:10 +0300

    Creating a text field

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,9 +3,13 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -26,6 +30,13 @@ fun TextFieldScreen() {
 }
 
 @Composable
+@Preview
 fun MyTextField() {
-    //TODO add your code here
-}
+   val textValue = remember {
+       mutableStateOf(value = "")
+   }
+TextField(value = textValue.value,
+    onValueChange ={textValue.value = it},
+label = {})
+   }
+
