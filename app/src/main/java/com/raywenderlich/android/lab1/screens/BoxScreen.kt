87edc1e19d77e75f1d84cfafee99ef33b807276c
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-22 21:17:06 +0300

    container Box

--- app/src/main/java/com/raywenderlich/android/lab1/screens/BoxScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/BoxScreen.kt
@@ -1,8 +1,16 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.RemeasurementModifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -17,6 +25,23 @@ fun BoxScreen(){
 }
 
 @Composable
-fun  MyBox(modifier: Modifier = Modifier){
+@Preview
+fun  MyBox(modifier: Modifier = Modifier,
+           contentModifier: Modifier = Modifier
+           ){
+    Box(modifier = modifier.fillMaxSize()){
+        Text(text = stringResource(id = R.string.first),
+        fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.TopStart)
 
+        )
+        Text(text = stringResource(id = R.string.second),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.Center)
+        )
+        Text(text = stringResource(id = R.string.third),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.BottomEnd)
+        )
+    }
 }
